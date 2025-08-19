9b37b81333b104862186163d8fc1d4abce99bd54
dxatelier-io <dxatelier.io@gmail.com>
2025-08-19 21:10:11 +0700

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,32 @@
+export default {
+  id: {
+    home: "Beranda",
+    about: "Tentang",
+    contact: "Kontak",
+    social: "Sosial Media",
+    portfolio: "Portfolio",
+    games: "Permainan",
+    library: "Perpustakaan",
+    bookMe: "Book Me Now!"
+  },
+  en: {
+    home: "Home",
+    about: "About",
+    contact: "Contact",
+    social: "Social Media",
+    portfolio: "Portfolio",
+    games: "Games",
+    library: "Library",
+    bookMe: "Book Me Now!"
+  },
+  fr: {
+    home: "Accueil",
+    about: "À propos",
+    contact: "Contact",
+    social: "Réseaux Sociaux",
+    portfolio: "Portfolio",
+    games: "Jeux",
+    library: "Bibliothèque",
+    bookMe: "Réservez-moi !"
+  }
+};
