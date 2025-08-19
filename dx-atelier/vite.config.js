fbb4cf589010ddb90beda5ddee0f4d36756e45b4
dxatelier-io <dxatelier.io@gmail.com>
2025-08-19 21:42:15 +0700

    Delete dx-atelier directory

--- dx-atelier/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ganti "dx-atelier" sesuai nama repo GitHub-mu
-export default defineConfig({
-  base: "/dx-atelier/",
-  plugins: [react()],
-})
