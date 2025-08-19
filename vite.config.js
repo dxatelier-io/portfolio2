a978f97d66db6ec5e89f53cee88db4ec99f2e662
dxatelier-io <dxatelier.io@gmail.com>
2025-08-19 21:24:27 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // ganti "dx-atelier" sesuai nama repo GitHub-mu
 export default defineConfig({
-  base: "/dx-atelier/",
+  base: "/portfol-io/",
   plugins: [react()],
 })
